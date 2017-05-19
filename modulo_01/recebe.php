b6800949d00071c929227405933431a80b018b32
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-05-19 20:29:32 -0300

    19/05/2017 20:29

---
+++ modulo_01/recebe.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Licença MIT
+ * Autor: Msc (c) Henrique Ferraz
+ * Data: 19 de maio de 2017
+ * henriqueferraz@ofnet.com.br
+ */
+if (isset($_POST['senha']) && !empty($_POST['senha'])) {
+    $senha = $_POST['senha'];
+    if ($_POST['senha'] === 'maria') {
+        echo 'Login efetuado com sucesso!!!';
+    } else {
+        echo 'Atenção ! Senha incorreta, login não efetuado';
+    }
+}
